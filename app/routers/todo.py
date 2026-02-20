57d3ff19234100f4f9f271f390590fced1cccd7d
Kwasi Edwards <kwasiedwards@gmail.com>
2026-02-20 10:38:27 -0400

    lab 5 base files updated

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Request, Response, Form
 from sqlmodel import select
 from app.database import SessionDep
 from app.models import *
@@ -6,6 +6,12 @@ from app.auth import encrypt_password, verify_password, create_access_token, Aut
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 from fastapi import status
+from fastapi.responses import HTMLResponse, RedirectResponse
+from app.utilities import flash
 
 todo_router = APIRouter(tags=["Todo Management"])
 
+@todo_router.post("/todos")
+def create_todo_action(request: Request, text: Annotated[str, Form()], db:SessionDep, user:AuthDep):
+    # Implement task 4.2 here. Remove the line below that says "pass" once complete
+    pass
